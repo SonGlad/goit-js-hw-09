d97e4b5fccd577c2e0fa92864580892ed2e8f05c
SonGlad <121556627+SonGlad@users.noreply.github.com>
2023-05-31 20:28:46 +0000

    Deploying to gh-pages from @ SonGlad/goit-js-hw-09@8b6ef1023c43568404a5cedd8526517e723ba135 🚀

---
+++ 01-color-switcher.0282e5c3.js
@@ -0,0 +1,2 @@
+const t={btnStartEl:document.querySelector("button[data-start]"),btnStopEl:document.querySelector("button[data-stop]")};let n=null;t.btnStartEl.addEventListener("click",(function(){if(n)return;n=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.btnStartEl.disabled=!0,t.btnStopEl.disabled=!1})),t.btnStopEl.addEventListener("click",(function(){n&&(clearInterval(n),t.btnStartEl.disabled=!1,t.btnStopEl.disabled=!0,n=null)}));
+//# sourceMappingURL=01-color-switcher.0282e5c3.js.map
