14c3a02b828a56add0aa15416e0fe78208b45522
SonGlad <okoshevy@gmail.com>
2023-05-31 23:37:01 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,6 @@
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
+};
 
 const refs = {
     btnStartEl: document.querySelector("button[data-start]"),
